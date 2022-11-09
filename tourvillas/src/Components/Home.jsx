3bf900e52fe0753950697ff23c643f0c92555b68
Moumita Das <dasmoumita.me@gmail.com>
2022-11-09 20:25:51 +0530

    adding navbar and search bar

---
+++ tourvillas/src/Components/Home.jsx
@@ -0,0 +1,24 @@
+// import { Container } from '@chakra-ui/react'
+import React from 'react'
+import Header from './Header'
+import Middle from './Middle'
+import Navbar from './Navbar'
+
+
+const Home = () => {
+  return (
+
+<>
+<Navbar/>
+<Header/>
+
+{/* <Middle/> */}
+
+
+</>
+
+
+  )
+}
+
+export default Home
