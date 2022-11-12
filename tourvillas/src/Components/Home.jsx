471f90644c7340cbb5936a12f480a8a00002bba1
Moumita Das <dasmoumita.me@gmail.com>
2022-11-12 18:22:35 +0530

    day-5

--- tourvillas/src/Components/Home.jsx
+++ tourvillas/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 // import { Container } from '@chakra-ui/react'
 import React from 'react'
+// import { Carasol } from './Carasol'
 import Header from './Header'
 import HomeFooter from './HomeFooter'
 import Maintwo from './Maintwo'
@@ -11,11 +12,11 @@ import Slidertwo from './Slidertwo'
 
 const Home = () => {
   return (
-
-<>
+  <>
 <Navbar/>
 <Header/>
 <Sliderr/>
+{/* <Carasol/> */}
 <Middlepage/>
 <Maintwo/>
 <Slidertwo/>
